0e765ccadd9fd1dfa0dc0679cc13d7b60508838e
Hugo1729 <hugobruwer4@gmail.com>
2026-09-08 10:48:29 +0200

    started work on bpe

---
+++ bpe.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+#excerpt from Hamlet act 1 scene 1
+text = """Enter Barnardo and Francisco, two sentinels Meeting
+
+BARNARDO Who's there?
+
+FRANCISCO Nay, answer me: stand and unfold yourself.
+
+BARNARDO Long live the king!
+
+FRANCISCO Barnardo?
+
+BARNARDO He.
+
+FRANCISCO You come most carefully upon your hour.
+
+BARNARDO 'Tis now struck twelve: get thee to bed, Francisco.
+
+FRANCISCO For this relief much thanks: 'tis bitter cold,
+And I am sick at heart.
+
+BARNARDO Have you had quiet guard?
+
+FRANCISCO Not a mouse stirring.
+
+BARNARDO Well, goodnight.
+If you do meet Horatio and Marcellus,
+The rivals of my watch, bid them make haste."""
+
+print([b for b in text.encode("utf-8")])
+print(5/0)
+
+def get_cnts(data):
+    ans = {}
+
+    for x, y in zip(data[:-1], data[1:]):
+        ans[(x,y)] = ans.get((x,y), 0) + 1
+
+    return ans
+
+def merge(data, token1, token2, token3):
+    ans = []
+
+    i = 0
+
+    while i < len(data):
+        if (i+1 < len(data) and data[i] == token1 and data[i+1] == token2):
+            ans.append(token3)
+            i += 2
+        else:
+            ans.append(data[i])
+            i += 1
+
+    return ans
+
+            
